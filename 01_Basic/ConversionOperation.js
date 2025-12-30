accb409221b9e94aa29532008777a82f2fdbc094
Ganesh Ghorpade <103923625+ganeshghorpade@users.noreply.github.com>
2025-12-30 11:23:32 +0000

    var,datatype,conversion operation

---
+++ 01_Basic/ConversionOperation.js
@@ -0,0 +1,24 @@
+let age = "22" // the age is int the form of str but we convert it in number
+console.log(typeof age);
+
+let numage = Number(age); // here we convert into the number
+console.log(typeof numage);
+
+let name = "Gnsesh"; // here the name in the string
+console.log(typeof name);
+
+let numbername = Number(name); // here convrt into into the number
+
+console.log(numbername); // output is NaN (not a number)
+
+let score = null
+console.log(typeof score);// output is object 
+
+
+let isLogin = 1;
+
+let booleanisLogin = Boolean(isLogin);
+
+console.log(booleanisLogin); // true
+
+console.log(typeof booleanisLogin); // Boolean
