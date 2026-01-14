2a11bf3878731a67812606224b753add4e72cdde
Ganesh <ganeshghorpade434@gmail.com>
2026-01-15 00:35:39 +0530

    map and reduce

--- 05_loop/map.js
+++ 05_loop/map.js
@@ -1,7 +1,18 @@
 let mynum = [1,2,3,4,5,6,7,8];
 
-let val = mynum.map((item)=>{
+/* let val = mynum.map((item)=>{
     return item + 10
 })
+ */
+//console.log(val);
 
-console.log(val);
+// also double map + filter we used
+// here by using the filter we find out the even value and with map we find square  of all even num
+// note . map is not used to search and remove the value so we used filter to extract only even value
+
+let result = mynum.filter((num)=> num%2===0).map((num)=>num*num);
+
+console.log(result);
+
+
+
--- 05_loop/reduce.js
+++ 05_loop/reduce.js
@@ -2,5 +2,38 @@
 
 let mynum = [1,2,3,4,5,6,7,8];
 
-my
+let result = mynum.reduce((acc,currval)=>{
+    //console.log(acc,currval);
+    
+   // return acc+currval;
+},1)
+
+// console.log(result);
+
+//-----------------------------------------------------------------------------------------
+
+const product_data = [
+    {
+        pname : "laptop 1",
+        price :120000
+    },
+    {
+        pname : "laptop 2",
+        price :28000
+    },
+    {
+        pname : "laptop 3",
+        price :20000
+    },
+    {
+        pname : "laptop 4",
+        price :130000
+    },
+]
+
+const totalcartprice = product_data.reduce((acc,curval)=> acc + curval.price,0);
+
+console.log(totalcartprice);
+
+
 
